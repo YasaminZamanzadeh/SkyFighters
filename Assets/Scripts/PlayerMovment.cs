f66d01d667ac0530d9451d784d49e4a26c4787b9
YasaminZamanzadeh <zamanzadehyasamin@yahoo.com>
2023-01-12 00:47:39 +0330

    Drift

--- Assets/Scripts/PlayerMovment.cs
+++ Assets/Scripts/PlayerMovment.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class PlayerMovment : MonoBehaviour
 {
+   public float rotationSpeed = 0.5f;
    public float speed = 3.5f;
    public Transform transform;
     void Start()
@@ -12,15 +13,28 @@ public class PlayerMovment : MonoBehaviour
     }
     void Update()
     {
+       Movment();
+       Clamp(); 
+    }
+   void Movment()
+  {
         if(Input.GetKey(KeyCode.RightArrow)){
            transform.position += new Vector3(speed * Time.deltaTime,0,0);
+           transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(0,0,80),rotationSpeed * Time.deltaTime);
              }
-
-         if(Input.GetKey(KeyCode.LeftArrow)){             
+        
+        if(Input.GetKey(KeyCode.LeftArrow)){             
            transform.position -=new Vector3(speed * Time.deltaTime,0,0);
+           transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(0,0,-80),rotationSpeed * Time.deltaTime);
             }
+        if(transform.rotation.z !=float.MaxValue ){
+        transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(0,0,0),rotationSpeed * Time.deltaTime);
+        }
 
-        Vector3 pos = transform.position;
+  }
+  void Clamp()
+  {
+      Vector3 pos = transform.position;
         pos.x = Mathf.Clamp(pos.x , -1.45f , 1.6f);
         transform.position= pos ;
   }

63d51de6ba9b209d3416a778330370ef54e4f391
YasaminZamanzadeh <zamanzadehyasamin@yahoo.com>
2023-01-18 15:07:00 +0330

    ModifyPoisons

--- Assets/Scripts/PotionSpawner.cs
+++ Assets/Scripts/PotionSpawner.cs
@@ -7,7 +7,7 @@ public class PotionSpawner : MonoBehaviour
      public GameObject [] potionsPrefabs;
     void Start()
     {
-        StartCoroutine(coinSpawner());
+        StartCoroutine(potionSpawner());
     }
 
     void Update()
@@ -15,18 +15,18 @@ public class PotionSpawner : MonoBehaviour
         
     }
 
-    void CoinSpawn(){
+    void potionSpawn(){
         int value = Random.Range(0,potionsPrefabs.Length);
         float rand = Random.Range(-1.64f , 1.78f);
         Instantiate(potionsPrefabs[value], new Vector3(rand,transform.position.y,transform.position.z),Quaternion.identity);
 
     }
-    IEnumerator coinSpawner() {
+    IEnumerator potionSpawner() {
         while(true)
         {
             int time = Random.Range(0,20);
-            yield return new WaitForSeconds(time);
-            CoinSpawn();
+            yield return new WaitForSeconds(2);
+            potionSpawn();
         }
     }
 }

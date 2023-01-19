abcd266f0211f1aa3874511472d789347697b25a
neginyahyaee <neginyahyaee@gmail.com>
2023-01-19 13:48:41 +0330

    LastScore

--- Assets/Scripts/ScoreManeger.cs
+++ Assets/Scripts/ScoreManeger.cs
@@ -2,42 +2,50 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement; 
 
 public class ScoreManeger : MonoBehaviour
-{ public int score = 0;
-public int highScore;
-   
+{ 
+    public int score = 0;
     public Text scoreText;
-  
+    public Text HighScoreText;
+    public Text lastScoreText;
+    public int highScore;
+    public static int lastScore = 0;
+
     void Start()
     {
         StartCoroutine(Score());
-        highScore = 0;
-
-        
+        // StartCoroutine(Reload());
+        highScore = PlayerPrefs.GetInt("high_score" , 0);
+        lastScore = PlayerPrefs.GetInt("last_score" , 0);
+        HighScoreText.text = "High Score: " + highScore.ToString();
+        lastScoreText.text = "Last Score: " + lastScore.ToString();
     }
 
-  
     void Update()
     {
-      scoreText.text = score.ToString();
-      if (score > highScore)
-      {
-        highScore = score;
-        Debug.Log(highScore);
-      }
-       }
+        scoreText.text = score.ToString();
+        if (score > highScore) {
+            highScore = score;
+            PlayerPrefs.SetInt("high_score", highScore);
         
-    
-    IEnumerator Score() {
-       while(true)
-       {
-        yield return new WaitForSeconds(2);
-        score = score + 1 ; 
-       
-     
-       }
-   }
-}
+        }
+    }
 
-  
+    IEnumerator Score() {
+        while(true){
+        yield return new WaitForSeconds(2);
+        score = score + 1;
+        lastScore = score;
+        PlayerPrefs.SetInt("last_score", lastScore);
+        }
+    }
+
+     IEnumerator Reload() {
+      
+        yield return new WaitForSeconds(10);
+        SceneManager.LoadScene("Game");
+
+        }
+    }
